8980c638ccc64ce47d01a1b11d4fa652c3fa8a34
Jorge Gonzalez <jorg3.594@gmail.com>
2020-05-26 22:11:42 -0500

    counter_cache

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,16 +1,4 @@
 class Review < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, counter_cache: :review_count
   belongs_to :reviewable, :polymorphic => true
-  after_create :created_update_count_review_user
-  after_destroy :destroy_update_count_review_user
-  
-  private
-  def update_count_review_user
-    user = User.find(self.user_id)
-    user.update(review_count: user.review_count + 1)
-  end
-  def destroy_update_count_review_user
-    user = User.find(self.user_id)
-    user.update(review_count: user.review_count - 1)
-  end
 end
