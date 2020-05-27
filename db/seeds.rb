7dfcf4fd017bbea300c3b4ba572fd1b3b75f95cc
Jorge Gonzalez <jorg3.594@gmail.com>
2020-05-27 00:53:32 -0500

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+
+def load_data (path)
+  JSON.parse(File.read(path))
+end
+
+company = load_data('db/companies.json')
+company.each { |company| Company.create!(company) }
+
+genres = load_data("db/genres.json")["genres"]
+genres.each { |genre| Genre.create!(name: genre) }
+
+platforms = load_data("db/platforms.json")
+platforms.each { |platform| Platform.create!(platform) }
+
+games = load_data("db/games.json")
+games.each do |game|
+  genres = Genre.where(name: game["genres"]) # Genre that match the names
+  game = Game.create!(game.slice("name", "summary", "release_date", "category", "rating"))
+  genres.each { |genre| game.genres << genre }
+end
