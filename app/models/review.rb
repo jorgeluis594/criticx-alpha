a49928d4978941ae138f706c73280449d1f41f75
Jorge Gonzalez <jorg3.594@gmail.com>
2020-05-26 21:24:40 -0500

    callback

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,16 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :reviewable, :polymorphic => true
+  after_create :created_update_count_review_user
+  after_destroy :destroy_update_count_review_user
+  
+  private
+  def update_count_review_user
+    user = User.find(self.user_id)
+    user.update(review_count: user.review_count + 1)
+  end
+  def destroy_update_count_review_user
+    user = User.find(self.user_id)
+    user.update(review_count: user.review_count - 1)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_26_181528) do
+ActiveRecord::Schema.define(version: 2020_05_27_015424) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -86,7 +86,7 @@ ActiveRecord::Schema.define(version: 2020_05_26_181528) do
     t.string "username"
     t.string "email"
     t.date "birth_date"
-    t.integer "review_count"
+    t.integer "review_count", default: 0, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
