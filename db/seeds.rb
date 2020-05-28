03f693e3c65b93251bb38fce2d635b6bf545049e
Jorge Gonzalez <jorg3.594@gmail.com>
2020-05-27 19:11:23 -0500

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,8 +14,15 @@ platforms = load_data("db/platforms.json")
 platforms.each { |platform| Platform.create!(platform) }
 
 games = load_data("db/games.json")
+
 games.each do |game|
   genres = Genre.where(name: game["genres"]) # Genre that match the names
-  game = Game.create!(game.slice("name", "summary", "release_date", "category", "rating"))
-  genres.each { |genre| game.genres << genre }
+  companies = game["involved_companies"]
+  data_game = game.slice("name", "summary", "release_date", "category", "rating")
+  data_game[:parent] = Game.where(name: game["parent"])[0] if game["category"] == 1
+  if game["rating"].is_a?(Integer)
+    game = Game.create!(data_game)
+    genres.each { |genre| game.genres << genre }
+    companies.each {|company| InvolvedCompany.create!(company: Company.where(name: company["name"])[0], game: game, developer: company["developer"], publisher: company["publisher"]) }
+  end
 end
